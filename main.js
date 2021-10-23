6c3b4f6859103004e40ded8dfa48107c585973b3
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2021-10-23 00:14:16 -0300

    criado o arquivo main.js e finalizado as ediçoes

---
+++ main.js
@@ -0,0 +1,72 @@
+//let name = "Ney Hiwerson";
+    //name = "o garotao";
+    //alert(name)
+    /*const links = {
+      github:"NeyHiwerson",
+      youtube : "UCWFhWfv5MqJKRvNCwYbYDtw",
+      facebook : "ney.hiwersonmissiasribeiro",
+      instagran : "hiwersonmissiasribeiro",
+      twitter : "hiwerson"
+    }*/
+    const links = {
+      github:"NeyHiwerson",
+      youtube : "patofuu",
+      facebook : "jonasbrothers",
+      instagran : "tunico e tinoco",
+      twitter : "hiwersongaranhão"
+    }
+    /*const github = "NeyHiwerson"
+    const youtube = "UCWFhWfv5MqJKRvNCwYbYDtw"
+    const facebook = "ney.hiwersonmissiasribeiro"
+    const instagran = "hiwersonmissiasribeiro"
+    const twitter = "hiwerson"*/
+
+    /*function showMeSometing(){
+      alert(links.instagran)
+    }
+
+    function changeSocialMediaLinks(){
+      document.getElementById('userName').textContent = 'Olivia'
+    }
+
+    changeSocialMediaLinks()
+
+    showMeSometing()*/
+    //alert(li.getAttribute('class'))
+
+    function changeSocialMediaLinks() {
+      //userName.textContent = "João e o Pé de feijão"
+      /*for (let i = 0; i <= 10; i++){
+        alert (i)
+      }*/
+      for (let li of socialLinks.children) {  
+        //alert(li.getAttribute('class'))      
+        /*const social = li.getAttribute('class')
+        alert(social)*/
+        //alert(li.children[0].href)
+        const social = li.getAttribute('class')
+        li.children[0].href = `https://${social}.com/${links[social]}`
+        //alert(li.children[0].href)
+      }
+    }
+    changeSocialMediaLinks()
+
+    function getGitHubProfileInfos(){
+      const url = `https://api.github.com/users/${links.github}`
+      //alert(url)
+
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+        //userLink.textContent = data.login
+        userLogin.textContent = data.login
+
+
+      })
+    }
+
+    getGitHubProfileInfos()
